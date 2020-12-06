f40bf3bcbdc02855fa016e8660abc4fb4dbbd928
Merlin Rock <rock.merlin@yahoo.com>
2020-12-06 18:47:23 -0500

    All save buttons working properly

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,148 @@ var currentDay = document.getElementById("currentDay");
 currentDay.textContent = "Today is " + moment().format("MMMM Do YYYY");
 
 
-var storageInputNote9 = document.getElementById("9AMnote");
-var noteValueFor9AM = storageInputNote9.value;
-console.log(noteValueFor9AM);
 var saveBtn9 = document.getElementById("save9AM");
+var saveBtn10 = document.getElementById("save10AM");
+var saveBtn11 = document.getElementById("save11AM");
+var saveBtn12 = document.getElementById("save12AM");
+var saveBtn1 = document.getElementById("save1AM");
+var saveBtn2 = document.getElementById("save2AM");
+var saveBtn3 = document.getElementById("save3AM");
+var saveBtn4 = document.getElementById("save4AM");
+var saveBtn5 = document.getElementById("save5AM");
+
+
+
+
 var storedNote9 = JSON.parse(localStorage.getItem("note9"))
+var storedNote10 = JSON.parse(localStorage.getItem("note10"))
+var storedNote11 = JSON.parse(localStorage.getItem("note11"))
+var storedNote12 = JSON.parse(localStorage.getItem("note12"))
+var storedNote1 = JSON.parse(localStorage.getItem("note1"))
+var storedNote2 = JSON.parse(localStorage.getItem("note2"))
+var storedNote3 = JSON.parse(localStorage.getItem("note3"))
+var storedNote4 = JSON.parse(localStorage.getItem("note4"))
+var storedNote5 = JSON.parse(localStorage.getItem("note5"))
 
 
 
-// if(storedNote9) {
-//     storageInputNote9.value = storedNote9
-// }
+saveBtn9.addEventListener("click", save9ToLocalStorage);
+saveBtn10.addEventListener("click", save10ToLocalStorage);
+saveBtn11.addEventListener("click", save11ToLocalStorage);
+saveBtn12.addEventListener("click", save12ToLocalStorage);
+saveBtn1.addEventListener("click", save1ToLocalStorage);
+saveBtn2.addEventListener("click", save2ToLocalStorage);
+saveBtn3.addEventListener("click", save3ToLocalStorage);
+saveBtn4.addEventListener("click", save4ToLocalStorage);
+saveBtn5.addEventListener("click", save5ToLocalStorage);
 
-var saveToLocalStorage = function() {
-    localStorage.setItem("note9", JSON.stringify(noteValueFor9AM));
-    console.log("here")
+
+if(storedNote9) {
+    var storageInputNote9 = document.getElementById("9AMnote");
+    storageInputNote9.value = storedNote9
 }
 
-saveBtn9.addEventListener("click", saveToLocalStorage);
+if(storedNote10) {
+    var storageInputNote10 = document.getElementById("10AMnote");
+    storageInputNote10.value = storedNote10
+}
+
+if(storedNote11) {
+    var storageInputNote11 = document.getElementById("11AMnote");
+    storageInputNote11.value = storedNote11
+}
+
+if(storedNote12) {
+    var storageInputNote12 = document.getElementById("12PMnote");
+    storageInputNote12.value = storedNote12
+}
+
+if(storedNote1) {
+    var storageInputNote1 = document.getElementById("1PMnote");
+    storageInputNote1.value = storedNote1
+}
+
+if(storedNote2) {
+    var storageInputNote2 = document.getElementById("2PMnote");
+    storageInputNote2.value = storedNote2
+}
+
+if(storedNote3) {
+    var storageInputNote3 = document.getElementById("3PMnote");
+    storageInputNote3.value = storedNote3
+}
+
+if(storedNote4) {
+    var storageInputNote4 = document.getElementById("4PMnote");
+    storageInputNote4.value = storedNote4
+}
+
+if(storedNote5) {
+    var storageInputNote5 = document.getElementById("5PMnote");
+    storageInputNote5.value = storedNote5
+}
+
+
+    
+function save9ToLocalStorage () {
+    var storageInputNote9 = document.getElementById("9AMnote");
+    var noteValueFor9AM = storageInputNote9.value;
+    console.log(noteValueFor9AM);
+    localStorage.setItem("note9", JSON.stringify(noteValueFor9AM));
+}
+
+function save10ToLocalStorage () {
+    var storageInputNote10 = document.getElementById("10AMnote");
+    var noteValueFor10AM = storageInputNote10.value;
+    console.log(noteValueFor10AM);
+    localStorage.setItem("note10", JSON.stringify(noteValueFor10AM));
+}
+
+function save11ToLocalStorage () {
+    var storageInputNote11 = document.getElementById("11AMnote");
+    var noteValueFor11AM = storageInputNote11.value;
+    console.log(noteValueFor11AM);
+    localStorage.setItem("note11", JSON.stringify(noteValueFor11AM));
+}
+
+function save12ToLocalStorage () {
+    var storageInputNote12 = document.getElementById("12PMnote");
+    var noteValueFor12PM = storageInputNote12.value;
+    console.log(noteValueFor12PM);
+    localStorage.setItem("note12", JSON.stringify(noteValueFor12PM));
+}
+
+function save1ToLocalStorage () {
+    var storageInputNote1 = document.getElementById("1PMnote");
+    var noteValueFor1PM = storageInputNote1.value;
+    console.log(noteValueFor1PM);
+    localStorage.setItem("note1", JSON.stringify(noteValueFor1PM));
+}
+
+function save2ToLocalStorage () {
+    var storageInputNote2 = document.getElementById("2PMnote");
+    var noteValueFor2PM = storageInputNote2.value;
+    console.log(noteValueFor2PM);
+    localStorage.setItem("note2", JSON.stringify(noteValueFor2PM));
+}
+
+function save3ToLocalStorage () {
+    var storageInputNote3 = document.getElementById("3PMnote");
+    var noteValueFor3PM = storageInputNote3.value;
+    console.log(noteValueFor3PM);
+    localStorage.setItem("note3", JSON.stringify(noteValueFor3PM));
+}
+
+function save4ToLocalStorage () {
+    var storageInputNote4 = document.getElementById("4PMnote");
+    var noteValueFor4PM = storageInputNote4.value;
+    console.log(noteValueFor4PM);
+    localStorage.setItem("note4", JSON.stringify(noteValueFor4PM));
+}
+
+function save5ToLocalStorage () {
+    var storageInputNote5 = document.getElementById("5PMnote");
+    var noteValueFor5PM = storageInputNote5.value;
+    console.log(noteValueFor5PM);
+    localStorage.setItem("note5", JSON.stringify(noteValueFor5PM));
+}
