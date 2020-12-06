baee6d63b416337fe00ff93aab83c89bc510c5a1
Merlin Rock <rock.merlin@yahoo.com>
2020-12-06 11:46:23 -0500

    Updated HTML container

--- Develop/script.js
+++ Develop/script.js
@@ -2,34 +2,3 @@ var currentDay = document.getElementById("currentDay");
 
 currentDay.textContent = "Today is " + moment().format("MMMM Do YYYY");
 
-// var scheduleSection = document.getElementById("container");
-// var unorderdList = document.createTextNode("ul");
-// var newHourSection = document.createElement("input");
-
-
-// for(i = 1; i <= 9; i++); {
-
-//     newHourSection.setAttribute("class", "hourSection")
-//     var textNode = document.createTextNode("Hello World!")
-//     console.log(textNode)
-//     newHourSection.appendChild(textNode);
-    
-//     scheduleSection.appendChild(newHourSection);
-//     // console.log("Got here " + i + " times!")
-// }
-
-
-
-// // newHourSection.setAttribute("id", "test");
-
-
-
-// // document.appendChild(newHourSection)
-// var scheduleSection = document.getElementById("container");
-//     var newHourSection = document.createElement("input");
-//     newHourSection.setAttribute("class", "")
-//     var textNode = document.createTextNode("Hello World!")
-//     console.log(textNode)
-//     newHourSection.appendChild(textNode);
-    
-//     scheduleSection.appendChild(newHourSection);
