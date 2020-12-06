efdc4d7893f22d050c64f7bcdea561d879105d66
Merlin Rock <rock.merlin@yahoo.com>
2020-12-06 17:41:52 -0500

    Update to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,22 @@
 var currentDay = document.getElementById("currentDay");
 currentDay.textContent = "Today is " + moment().format("MMMM Do YYYY");
 
-document.getElementById("save9AM").addEventListener("click", function() {
-    document.getElementById("9AMnote").innerHTML;
-    // var noteFor9AM = noteFor9AM.innerHTML;
-    console.log(document.getElementById("9AMnote").innerHTML)
-    console.log("Made it here!")
 
-});
+var storageInputNote9 = document.getElementById("9AMnote");
+var noteValueFor9AM = storageInputNote9.value;
+console.log(noteValueFor9AM);
+var saveBtn9 = document.getElementById("save9AM");
+var storedNote9 = JSON.parse(localStorage.getItem("note9"))
+
+
+
+// if(storedNote9) {
+//     storageInputNote9.value = storedNote9
+// }
+
+var saveToLocalStorage = function() {
+    localStorage.setItem("note9", JSON.stringify(noteValueFor9AM));
+    console.log("here")
+}
+
+saveBtn9.addEventListener("click", saveToLocalStorage);
