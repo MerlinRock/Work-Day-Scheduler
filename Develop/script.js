18abb329e9d4b01e21adcc0c70e94fbdad1ee8dc
Merlin Rock <rock.merlin@yahoo.com>
2020-12-06 15:08:58 -0500

    Updated HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,10 @@
 var currentDay = document.getElementById("currentDay");
-
 currentDay.textContent = "Today is " + moment().format("MMMM Do YYYY");
 
+document.getElementById("save9AM").addEventListener("click", function() {
+    document.getElementById("9AMnote").innerHTML;
+    // var noteFor9AM = noteFor9AM.innerHTML;
+    console.log(document.getElementById("9AMnote").innerHTML)
+    console.log("Made it here!")
+
+});
